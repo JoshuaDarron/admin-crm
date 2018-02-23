a310983773b0220e923f103f848b3ca4c77747ef
Joshua Spears <j.dspears@yahoo.com>
2018-02-23 11:31:23 -0800

    Added the styling of choice for the form on the create an employee page.

--- app_client/src/components/Form/Form.js
+++ app_client/src/components/Form/Form.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 // IMPORT STATE FROM HELPERS FOLDER
 import FormStates from "../../helpers/FormStates";
 
-// FORM COMPONENT 
+// FORM COMPONENT
 class Form extends Component {
     // RENDER
     render() {
@@ -15,56 +15,77 @@ class Form extends Component {
         return (
             <div>
                 <form>
-                    <div className="form-group">
-                        <label htmlFor="firstName">First Name</label>
-                        <input
-                            readOnly={readOnly}
-                            value={this.props.employee.firstName}
-                            type="text"
-                            className="form-control"
-                            name="firstName"
-                            placeholder="First Name..."
-                            onChange={this.props.handleChange}/>
+                    {/* FIRST NAME */}
+                    <div className="form-group row">
+                        <label htmlFor="firstName" className="col-sm-2 col-form-label">
+                            First Name
+                        </label>
+                        <div className="col-sm-10">
+                            <input
+                                readOnly={readOnly}
+                                value={this.props.employee.firstName}
+                                type="text"
+                                className="form-control"
+                                name="firstName"
+                                placeholder="First Name..."
+                                onChange={this.props.handleChange}/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="lastName">Last Name</label>
-                        <input
-                            readOnly={readOnly}
-                            value={this.props.employee.lastName}
-                            type="text"
-                            className="form-control"
-                            name="lastName"
-                            placeholder="Last Name..."
-                            onChange={this.props.handleChange}/>
+                    {/* LAST NAME */}
+                    <div className="form-group row">
+                        <label htmlFor="lastName" className="col-sm-2 col-form-label">
+                            Last Name
+                        </label>
+                        <div className="col-sm-10">
+                            <input
+                                readOnly={readOnly}
+                                value={this.props.employee.lastName}
+                                type="text"
+                                className="form-control"
+                                name="lastName"
+                                placeholder="Last Name..."
+                                onChange={this.props.handleChange}/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="age">Age</label>
-                        <input
-                            readOnly={readOnly}
-                            value={this.props.employee.age}
-                            type="text"
-                            className="form-control"
-                            name="age"
-                            placeholder="Age..."
-                            onChange={this.props.handleChange}/>
+                    {/* AGE */}
+                    <div className="form-group row">
+                        <label htmlFor="age" className="col-sm-2 col-form-label">
+                            Age
+                        </label>
+                        <div className="col-sm-10">
+                            <input
+                                readOnly={readOnly}
+                                value={this.props.employee.age}
+                                type="text"
+                                className="form-control"
+                                name="age"
+                                placeholder="Age..."
+                                onChange={this.props.handleChange}/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="jobTitle">Job Title</label>
-                        <input
-                            readOnly={readOnly}
-                            value={this.props.employee.jobTitle}
-                            type="text"
-                            className="form-control"
-                            name="jobTitle"
-                            placeholder="Job Title..."
-                            onChange={this.props.handleChange}/>
+                    {/* JOB TITLE */}
+                    <div className="form-group row">
+                        <label htmlFor="jobTitle" className="col-sm-2 col-form-label">
+                            Job Title
+                        </label>
+                        <div className="col-sm-10">
+                            <input
+                                readOnly={readOnly}
+                                value={this.props.employee.jobTitle}
+                                type="text"
+                                className="form-control"
+                                name="jobTitle"
+                                placeholder="Job Title..."
+                                onChange={this.props.handleChange}/>
+                        </div>
                     </div>
+                    {/* SUBMIT BUTTON */}
                     <div className="clearfix">
                         <button
                             onClick={this.props.handleSubmit}
                             className="btn btn-primary pull-right"
                             type="submit">
-                                Create
+                            Create
                         </button>
                     </div>
 
